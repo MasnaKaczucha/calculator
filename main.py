44d96b4c4f0d6783a66d3cdacf3afdc522dff3f3
Sebastian Kwiecien <kwiecse21@sps-prosek.cz>
2023-03-30 20:39:59 +0200

    done

--- main.py
+++ main.py
@@ -15,23 +15,33 @@ for a in range(rownum):
         root.columnconfigure(b, weight=1)
 
 
-entry = tkinter.Entry(root)
+operation = tkinter.StringVar()
+entry = tkinter.Entry(root, textvariable=operation, font=('Rosewood Std Regular', 15),)
 entry.grid(row=0, column=0, columnspan=4, sticky="nsew")
 
-calc = ""
-# lmfao
-def count(value):
-    global entry
-    global calc
 
-    entry.insert("end", value)
-    calc += value
+def count(text):
+    entry.insert("end", text)
 
-    return calc
+
+def outcome():
+    var = entry.get()
+    entry.delete(0, "end")
+    try:
+        test = eval(var)
+    except:
+        entry.insert("end", "unlucky")
+    else:
+        print(operation, "akldfj")
+        entry.insert("end", test)
+
+
+def ac():
+    entry.delete(0, "end")
 
 
 def create_button(num):
-    content = (tkinter.Button(root, text=num, command=lambda: count(str(num))))
+    content = (tkinter.Button(root, font=('Rosewood Std Regular', 15), text=num, command=lambda: count(str(num))))
     button.append(content)
 
 
@@ -53,31 +63,32 @@ button[2].grid(row=4, column=1, sticky="nsew")
 button[3].grid(row=4, column=2, sticky="nsew")
 button[0].grid(row=5, column=0, columnspan=2, sticky="nsew")
 
-clr_all = tkinter.Button(root, text="AC")
+clr_all = tkinter.Button(root, font=('Rosewood Std Regular', 15), text="AC", command=ac)
 clr_all.grid(row=1, column=0, sticky="nsew")
 
-exp = tkinter.Button(root, text="x^2")
+exp = tkinter.Button(root, font=('Rosewood Std Regular', 15), text="x^2", command=lambda: count('**2'))
 exp.grid(row=1, column=1, sticky="nsew")
 
-sqrt = tkinter.Button(root, text="√")
+sqrt = tkinter.Button(root, font=('Rosewood Std Regular', 15), text="√", command=lambda: count('**(1/2)'))
 sqrt.grid(row=1, column=2, sticky="nsew")
 
-div = tkinter.Button(root, text="÷")
+div = tkinter.Button(root, font=('Rosewood Std Regular', 15), text="÷", command=lambda: count('/'))
 div.grid(row=1, column=3, sticky="nsew")
 
-mult = tkinter.Button(root, text="X")
+mult = tkinter.Button(root, font=('Rosewood Std Regular', 15), text="X", command=lambda: count('*'))
 mult.grid(row=2, column=3, sticky="nsew")
 
-sub = tkinter.Button(root, text="-")
+sub = tkinter.Button(root, font=('Rosewood Std Regular', 15), text="-", command=lambda: count('-'))
 sub.grid(row=3, column=3, sticky="nsew")
 
-add = tkinter.Button(root, text="+")
+add = tkinter.Button(root, font=('Rosewood Std Regular', 15), text="+", command=lambda: count('+'))
 add.grid(row=4, column=3, sticky="nsew")
 
-result = tkinter.Button(root, text="=")
+result = tkinter.Button(root, font=('Rosewood Std Regular', 15), text="=", command=lambda: outcome())
 result.grid(row=5, column=3, sticky="nsew")
 
-point = tkinter.Button(root, text=",")
+point = tkinter.Button(root, font=('Rosewood Std Regular', 15), text=",", command=lambda: (count('.')))
 point.grid(row=5, column=2, sticky="nsew")
+
 
 tkinter.mainloop()
