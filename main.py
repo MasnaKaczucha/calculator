510fc8313c28ab32c22e77098339b5f51cdd455b
kwiecse21 <kwiecse21@sps-prosek.cz>
2023-03-30 09:34:08 +0200

    fml

--- main.py
+++ main.py
@@ -1,46 +1,83 @@
 import tkinter
 
-
-def addition():
-    print('add shit')
-
-
-def subtraction():
-    print('subtract shit')
-
-
-def division():
-    print('divide shit')
-
-
-def multiplication():
-    print('multiply shit')
-
-
-def exponentiation():
-    print('to the power of shit')
-
-
-def square_root():
-    print('square root of shit')
-
-
 root = tkinter.Tk()
-root.geometry('500x500')
+root.geometry('200x300')
+
 
 nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+rownum = 6
+colnum = 4
+
+for a in range(rownum):
+    for b in range(colnum):
+        root.rowconfigure(a, weight=1)
+        root.columnconfigure(b, weight=1)
+
+
+entry = tkinter.Entry(root)
+entry.grid(row=0, column=0, columnspan=4, sticky="nsew")
+
+calc = ""
+
+def count(value):
+    global entry
+    global calc
+
+    entry.insert("end", value)
+    calc += value
+
+    return calc
+
+
+def create_button(num):
+    content = (tkinter.Button(root, text=num, command=lambda: count(str(num))))
+    button.append(content)
+
+
 button = []
 for num in nums:
-    button.append(tkinter.Button(root, text=num))
+    create_button(num)
 
-button[7].grid(row=0, column=0, sticky="w", pady=2)
-button[8].grid(row=0, column=1, sticky="w", pady=2)
-button[9].grid(row=0, column=2, sticky="w", pady=2)
 
-button[4].grid(row=1, column=0, sticky="w", pady=2)
-button[5].grid(row=1, column=1, sticky="w", pady=2)
-button[6].grid(row=1, column=2, sticky="w", pady=2)
+button[7].grid(row=2, column=0, sticky="nsew")
+button[8].grid(row=2, column=1, sticky="nsew")
+button[9].grid(row=2, column=2, sticky="nsew")
 
+button[4].grid(row=3, column=0, sticky="nsew")
+button[5].grid(row=3, column=1, sticky="nsew")
+button[6].grid(row=3, column=2, sticky="nsew")
+
+button[1].grid(row=4, column=0, sticky="nsew")
+button[2].grid(row=4, column=1, sticky="nsew")
+button[3].grid(row=4, column=2, sticky="nsew")
+button[0].grid(row=5, column=0, columnspan=2, sticky="nsew")
+
+clr_all = tkinter.Button(root, text="AC")
+clr_all.grid(row=1, column=0, sticky="nsew")
+
+exp = tkinter.Button(root, text="x^2")
+exp.grid(row=1, column=1, sticky="nsew")
+
+sqrt = tkinter.Button(root, text="√")
+sqrt.grid(row=1, column=2, sticky="nsew")
+
+div = tkinter.Button(root, text="÷")
+div.grid(row=1, column=3, sticky="nsew")
+
+mult = tkinter.Button(root, text="X")
+mult.grid(row=2, column=3, sticky="nsew")
+
+sub = tkinter.Button(root, text="-")
+sub.grid(row=3, column=3, sticky="nsew")
+
+add = tkinter.Button(root, text="+")
+add.grid(row=4, column=3, sticky="nsew")
+
+result = tkinter.Button(root, text="=")
+result.grid(row=5, column=3, sticky="nsew")
+
+point = tkinter.Button(root, text=",")
+point.grid(row=5, column=2, sticky="nsew")
 
 tkinter.mainloop()
